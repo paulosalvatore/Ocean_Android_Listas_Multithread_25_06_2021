51d150aa30ef83c3ebc8fff60dfd28e4896693d0
Paulo Salvatore <salvatoregames@gmail.com>
2021-06-25 11:32:55 -0300

    Criação do Adapter (WIP)

---
+++ app/src/main/java/com/oceanbrasil/ocean_android_listas_multithread_25_06_2021/ItemAdapter.kt
@@ -0,0 +1,22 @@
+package com.oceanbrasil.ocean_android_listas_multithread_25_06_2021
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class ItemAdapter : RecyclerView.Adapter<ItemAdapter.ViewHolder>() {
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
